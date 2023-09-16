46d1c4d93650486be4a4858d417fc5f6173fd087
jwang5953 <wang5953@purdue.edu>
2023-09-16 18:30:04 -0400

    Create Server.java

---
+++ .idea/src/Server.java
@@ -0,0 +1,14 @@
+import java.net.*;
+import java.io.*;
+import java.util.HashMap;
+
+public class Server {
+
+    public static final int[] SERVER_PORTS = {5001, 5002, 5003, 5004, 5005};
+    public static final String SERVER_IP = "";
+    public static final String SERVER_NAME = "";
+
+    public static void main(String[] args){
+
+    }
+}
